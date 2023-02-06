e516f366a781c360ceb05e4ff8eb0bcfab543cbf
rajeshwiper123 <77289809+rajeshwiper123@users.noreply.github.com>
2023-02-06 14:58:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.println("*************This is a simple Java program.*****************");
+		System.out.println("*************Made some changes.*****************");
 	}
 }
